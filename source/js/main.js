ee16c25811cac45dd0702259e295ee9114df3fb7
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-09-12 16:00:16 +0300

    Промежуточное сохранение

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,25 @@
 // https://swiperjs.com/get-started#installation
-import Swiper from "swiper";
-import {Navigation, Pagination} from "swiper/modules";
+import Swiper from 'swiper';
+import {Navigation, Pagination} from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/pagination';
 import './menu';
+
+new Swiper('.hero__swiper', {
+  modules: [Pagination],
+  autoHeight:true,
+  loop: true,
+  pagination: {
+    el: '.hero__swiper-pagination',
+    clickable: true,
+    bulletElement: 'button',
+    renderBullet: function (index, className) {
+      return `<button class="${className}" type="button"><span class="visually-hidden">Элемент пагинации ${index + 1}</span></button>`;
+    },
+  },
+  breakpoints: {
+    1440: {
+      allowTouchMove: false
+    }
+  }
+});
