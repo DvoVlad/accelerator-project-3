a37e5d95ee1eb13640a4e72f8ade7fbd0925a397
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-09-16 14:38:50 +0300

    Сделал модалку кроме маски

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,11 +1,15 @@
+const modalClose = document.querySelector('.about__modal-close');
 const modalOpenButton = document.querySelector('.about__button-link');
 const modalOverlay = document.querySelector('.about__modal-overlay');
 const modal = document.querySelector('.about__modal');
 
-modalOverlay.addEventListener('click', () => {
+const closeModal = () => {
   modalOverlay.classList.remove('about__modal-overlay--open');
   modal.classList.remove('about__modal--open');
-});
+};
+
+modalOverlay.addEventListener('click', closeModal);
+modalClose.addEventListener('click', closeModal);
 
 modalOpenButton.addEventListener('click', () => {
   modalOverlay.classList.add('about__modal-overlay--open');
